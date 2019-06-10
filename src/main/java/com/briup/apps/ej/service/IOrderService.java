805c021660fe877eea9878951ead73d77e572cca
junjun955 <529665956@qq.com>
2019-06-10 17:56:49 +0800

    赵丽君的Order和Order_line表格相关增删改查的第一次提交

---
+++ src/main/java/com/briup/apps/ej/service/IOrderService.java
@@ -0,0 +1,18 @@
+package com.briup.apps.ej.service;
+
+import com.briup.apps.ej.bean.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    List<Order> query(Order order);
+
+    List<Order> findAll();
+
+    Order findById(long id);
+
+    void saveOrUpdate(Order order) throws Exception ;
+
+    void deleteById(long id) throws Exception ;
+
+}
