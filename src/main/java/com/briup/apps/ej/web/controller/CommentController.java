45d60bd7216755d6417263683b218036b7035df0
qpl123 <1695521701@qq.com>
2019-06-14 09:38:13 +0800

    comment页面修改

--- src/main/java/com/briup/apps/ej/web/controller/CommentController.java
+++ src/main/java/com/briup/apps/ej/web/controller/CommentController.java
@@ -1,18 +1,19 @@
 package com.briup.apps.ej.web.controller;
 import com.briup.apps.ej.bean.Comment;
-import com.briup.apps.ej.bean.Customer;
 import com.briup.apps.ej.service.ICommentService;
 import com.briup.apps.ej.utils.Message;
 import com.briup.apps.ej.utils.MessageUtil;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Api(description = "顾客评论管理相关接口")
 @Validated
 @RestController
 @RequestMapping("/comment")
@@ -31,21 +32,21 @@ public class CommentController {
 
     @PostMapping("saveOrUpdate")
     @ApiOperation("保存或者更新顾客信息")
-    public Message saveOrUpdate(Comment comment) throws Exception{
+    public Message saveOrUpdate(@Valid @ModelAttribute Comment comment) throws Exception{
         commentService.saveOrUpdate(comment);
         return MessageUtil.success("操作成功");
     }
 
     @GetMapping("deleteById")
     @ApiOperation("通过ID删除")
-    public Message deleteById(Long id) throws Exception{
+    public Message deleteById(@NotNull @RequestParam("id")Long id) throws Exception{
         commentService.deleteById(id);
         return MessageUtil.success("删除成功");
     }
 
     @PostMapping("batchDelete")
     @ApiOperation("批量删除顾客信息")
-    public Message batchDelete(@NotNull(message = "ids不能为空") long[] ids) throws Exception{
+    public Message batchDelete(long[] ids) throws Exception{
         commentService.batchDelete(ids);
         return MessageUtil.success("批量删除成功");
     }
