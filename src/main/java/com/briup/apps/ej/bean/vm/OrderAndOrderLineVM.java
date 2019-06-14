40185b8576d9d05356f8d825825a67d5c06be97d
lululu0 <1272741817@qq.com>
2019-06-14 08:43:35 +0800

    吕凯璐Order和OrderLine的级联查询

---
+++ src/main/java/com/briup/apps/ej/bean/vm/OrderAndOrderLineVM.java
@@ -0,0 +1,38 @@
+package com.briup.apps.ej.bean.vm;
+
+import com.briup.apps.ej.bean.Order_line;
+import io.swagger.annotations.ApiParam;
+
+import java.util.List;
+
+public class OrderAndOrderLineVM {
+    @ApiParam(value = "顾客ID",required = true)
+    private Long customerId;
+    @ApiParam(value = "服务地址ID",required = true)
+    private Long addressId;
+    private List<Order_line> orderLines;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public List<Order_line> getOrderLines() {
+        return orderLines;
+    }
+
+    public void setOrderLines(List<Order_line> orderLines) {
+        this.orderLines = orderLines;
+    }
+}
